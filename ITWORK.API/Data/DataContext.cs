c4976e09350ebbeb5a1c181227276c0da50bc4d7
Luka Tarkhnishvili <lukatarkhnishvili@gmail.com>
2019-09-16 17:37:51 +0400

    Initial commit

---
+++ ITWORK.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using ITWORK.API.Modules;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITWORK.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+    
+        public DbSet<Value> Values { get; set; }
+    }
+}
